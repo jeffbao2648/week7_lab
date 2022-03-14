25624239e0e2da1bd7bef829e8f674d8bd4d7635
jeffbao2648 <99672792+jeffbao2648@users.noreply.github.com>
2022-03-14 14:36:00 -0400

    Update main.js

--- main.js
+++ main.js
@@ -56,4 +56,8 @@ function generateRandomAnimal() {
 
 function onLoad() {
   var animal = generateRandomAnimal(); 
+  document.getElementById("animal_information").textContent = animal.name + "( " + animal.age + "years old )";
+  var imageId = document.getElementById("animal_image");
+  imageTag.setAttribute("src", animal.image); 
+  imageTag.setAttribute("alt", animal.image_alt);
 }
